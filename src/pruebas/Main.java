cbff20a24584c6bb0d11bfdba1d0150c27e1e094
Mapakitus <mapakitus@gmail.com>
2025-05-02 10:22:47 +0200

    Creada clase main con "Hola Mundo"

---
+++ src/pruebas/Main.java
@@ -0,0 +1,10 @@
+package pruebas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo!");
+        // Aquí puedes agregar más lógica o pruebas según sea necesario.
+    }
+
+}
